9cbdd3ed9088561f994f1d2190c7b66bb1372f06
Adithya Ajith <4adhi007@gmail.com>
2021-12-05 16:46:20 +0530

    adding docs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 // create element & render cafe
 function renderCafe(doc){
@@ -21,3 +22,13 @@ db.collection('cafes').get().then((snapshot)=>{
         
     });
 })
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
+});
